e7b6b4c323db631a1949bef84efc22560b3500a5
Dennis David <dhavid-arango77@hotmail.com>
2020-09-02 13:31:49 -0500

    Proyecto con lista de categorias y lista de movies

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import {
+  Text,
+} from 'react-native';
+
+import SuggestionList from './src/videos/containers/suggestion-list';
+import API from './utils/api';
+import CategoryList from './src/videos/containers/category-list.js';
 import Home from './src/screens/containers/Home'
-import Header from './src/sections/components/Header'
-import SuggestionList from './src/videos/containers/suggestion-list'
+import  Header  from './src/sections/components/Header';
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    suggestionList: [],
+    categoryList: [],
+  }
+  async componentDidMount() {
+    const movies = await API.getSuggestion(10);
+    const categories = await API.getMovies();
+    console.log(movies);
+    console.log(categories);
+    this.setState({
+      suggestionList: movies,
+      categoryList: categories,
+    })
+  }
   render() {
     return (
       <Home>
-        <Header>
-          <Text>Close Session</Text>
-        </Header>
-        <Text>Buscador</Text>
-        <Text>Categorias</Text>
-        <SuggestionList/>
-
+         <Header/>
+        <Text>buscador</Text>
+        <Text>categorías</Text>
+        <CategoryList
+          list={this.state.categoryList}
+        />
+        <SuggestionList
+          list={this.state.suggestionList}
+        />
       </Home>
+       
+      
     );
   }
 }
+
